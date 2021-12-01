403e30c87ff843bada40bdea97cdbe5823035f23
Hasan Ahmad <91635325+Hasan-CS@users.noreply.github.com>
2021-12-01 15:51:24 -0800

    Add files via upload

--- mini_golf.py
+++ mini_golf.py
@@ -10,6 +10,7 @@ ball = trtl.Turtle()
 sand_one = trtl.Turtle()
 sand_two = trtl.Turtle()
 sand_three = trtl.Turtle()
+sand_four = trtl.Turtle()
 pond_one = trtl.Turtle()
 pond_two = trtl.Turtle()
 hole = trtl.Turtle()
@@ -38,6 +39,7 @@ def move(turtle, xx, yy):
 def draw_map_one():
     pond_two.hideturtle()
     sand_three.hideturtle()
+    sand_four.hideturtle()
 
     move(ball, -300, -200)
     move(hole, 300, 200)
@@ -75,6 +77,7 @@ def draw_map_one():
 
 def draw_map_three():
     sand_three.hideturtle()
+    sand_four.hideturtle()
 
     move(ball, 0, -200)
     move(hole, -300, 180)
@@ -117,18 +120,52 @@ def draw_map_three():
         else:
             draw.fd(75)
 
+def draw_map_four():
 
+    move(ball, 0, -100)
+    move(hole, 0, 100)
+    move(sand_one, 200, -40)
+    move(sand_two, 70, 180)
+    move(sand_three, -230, -100)
+    move(sand_four, -100, 25)
+    move(pond_one, -240, 180)
+    move(pond_two, 240, -160)
+    move(draw, 0, 0)
+
+
+    draw.rt(45)
+    for i in range(2):
+        draw.fd(125)
+        draw.bk(250)
+        draw.fd(125)
+        draw.rt(90)
+    
+    move(draw, -350, -220)
+    draw.rt(45)
+    for i in range(2):
+        draw.fd(225)
+        draw.rt(90)
+        draw.fd(75)
+        draw.bk(75)
+        draw.lt(90)
+        draw.fd(225)
+
+        draw.rt(90)
+        draw.fd(700)
+        draw.rt(90)
 
 # game
 draw_turtle(ball, "golf_ball.gif")
 draw_turtle(sand_one, "sand.gif")
 draw_turtle(sand_two, "sand.gif")
 draw_turtle(sand_three, "sand.gif")
+draw_turtle(sand_four, "sand.gif")
 draw_turtle(pond_one, "pond.gif")
 draw_turtle(pond_two, "pond.gif")
 draw_turtle(hole, "hole.gif")
 
 #draw_map_one()
-draw_map_three()
+#draw_map_three()
+#draw_map_four()
 
 wn.mainloop()
